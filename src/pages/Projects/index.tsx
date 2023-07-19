1c4a5e9707aef0ba2c90ac3b3155dfdbe5271703
moleksandr <digit.maestro@outlook.com>
2023-07-20 00:07:08 +0300

    Create widget component.

--- src/pages/Projects/index.tsx
+++ src/pages/Projects/index.tsx
@@ -5,6 +5,8 @@ import React, { useState } from 'react';
 import { MainLayout } from '../../components/layouts/MainLayout';
 import { Section } from '../../components/Section';
 import {useProjectContext} from "../../contexts/ProjectContext";
+import { DndProvider } from 'react-dnd';
+import { HTML5Backend } from 'react-dnd-html5-backend';
 
 // Export page
 const ProjectsPage = () => {
@@ -12,9 +14,12 @@ const ProjectsPage = () => {
 
   return (
     <MainLayout>
+      <DndProvider backend={HTML5Backend}>
       {sections.map(section => (
         <Section key={section.id} id={section.id} />
       ))}
+      </DndProvider>
+
       <div className="w-full h-px border border-indigo-500 relative">
         <button className='absolute left-1/2 top-1/2 translate-x-[-50%] translate-y-[-50%] w-40 h-9 bg-indigo-500 rounded-3xl text-white' onClick={addSection}>+ Add Section</button>
       </div>

d5e2f8a5c4f7a071e06bf07c5fe6336918349334
moleksandr <digit.maestro@outlook.com>
2023-08-03 05:09:40 -0700

    Frontend of Files

--- src/constants/routes.ts
+++ src/constants/routes.ts
@@ -1,7 +1,9 @@
 export const LOGIN = '/login'
 export const HOME = '/home'
+export const Files ='/Files'
 export const TASKS = '/tasks'
 export const PROJECTS = '/projects'
 export const SIGN_UP = '/register'
+
 
 export const VERIFICATION_DONE = '/verification-done'
